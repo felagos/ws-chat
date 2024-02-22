f484ea6c2c24e77447fe0e52bf32e8b6dde57ac0
Felipe Andres Lagos Sanchez <flagos@internacional.cl>
2024-02-22 16:54:27 -0300

    Add message controller and token validation middleware

--- server/src/router/message.router.ts
+++ server/src/router/message.router.ts
@@ -1,3 +1,7 @@
 import { Router } from 'express';
+import { tokenValidation } from '../middlewares/token.middleware';
+import { getChats } from '../controllers';
 
 export const messageRouter = Router();
+
+messageRouter.get('/:from', tokenValidation, getChats);
