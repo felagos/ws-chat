a19cb50d87df612ba5ad3194469c703c18655da1
Felipe Andres Lagos Sanchez <flagos@internacional.cl>
2024-02-26 12:37:57 -0300

    Fix formatting in http.ts

--- client/src/http/http.ts
+++ client/src/http/http.ts
@@ -1,28 +1,28 @@
 const fetchWithHeaders = async <T>(url: string, options: RequestInit = {}): Promise<T> => {
-  const token = localStorage.getItem('token');
+	const token = localStorage.getItem('token');
 
-  options.headers = {
-    'Content-Type': 'application/json',
-    ...options.headers,
-  };
+	options.headers = {
+		'Content-Type': 'application/json',
+		...options.headers,
+	};
 
-	if(token) {
+	if (token) {
 		options.headers = {
 			...options.headers,
 			'Authorization': `Bearer ${token}`,
 		};
 	}
 
-  return fetch(url, options) as T;
+	return fetch(url, options) as T;
 };
 
-const get = <T>(url: string, options: RequestInit = {}): Promise<T> => 
+const get = <T>(url: string, options: RequestInit = {}): Promise<T> =>
 	fetchWithHeaders(url, { ...options, method: 'GET' });
 
-const post = <T = unknown>(url: string, body: unknown, options: RequestInit = {}): Promise<T> => 
+const post = <T = unknown>(url: string, body: unknown, options: RequestInit = {}): Promise<T> =>
 	fetchWithHeaders(url, { ...options, method: 'POST', body: JSON.stringify(body) });
 
-const put = <T = unknown>(url: string, body: unknown, options: RequestInit = {}): Promise<T> => 
+const put = <T = unknown>(url: string, body: unknown, options: RequestInit = {}): Promise<T> =>
 	fetchWithHeaders(url, { ...options, method: 'PUT', body: JSON.stringify(body) });
 
 
