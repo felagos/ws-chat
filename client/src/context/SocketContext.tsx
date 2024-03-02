e2e85f6001952d0531da6953480198c0c5bf8a3e
Felipe Lagos <felagossanchez@gmail.com>
2024-03-02 17:45:31 -0300

    Add user authentication to SocketProvider

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -1,7 +1,8 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useEffect } from "react";
 import { useSocket } from "../hooks";
 import { Socket } from "socket.io-client";
 import env from "../env";
+import { useAuthStore } from "../store";
 
 const URL_SOCKET = env.SOCKET_URL;
 
@@ -19,7 +20,14 @@ interface Props {
 export const SocketContext = createContext({} as SocketContextProps);
 
 export const SocketProvider = ({ children }: Props) => {
+	const user = useAuthStore(state => state.user);
 	const { socket, isOnline, connectSocket, disconnectSocket } = useSocket(URL_SOCKET);
+
+	useEffect(() => {
+		const socketCB = user ? connectSocket : disconnectSocket;
+		
+		socketCB();
+	}, [connectSocket, disconnectSocket, user]);
 
 	const value: SocketContextProps = {
 		socket,
